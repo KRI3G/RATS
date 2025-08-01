dd18bd075c83378401eceb9257528d8ed778306c
KRI3G <kevin.guerra.v@gmail.com>
2025-07-31 21:53:39 -0500

    Added test config and docker compose

---
+++ gateway/generate_keys.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ssh-keygen -q -N "" -t rsa -b 4096 -f ssh_host_rsa_key
+ssh-keygen -q -N "" -t ed25519 -f ssh_host_ed25519_key
+
+# Public key files are not needed
+rm ssh_host_*_key.pub
