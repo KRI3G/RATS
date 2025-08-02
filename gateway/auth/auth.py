60d016c6866cd4827e99ca5055a68136a6dc4811
KRI3G <kevin.guerra.v@gmail.com>
2025-08-02 02:43:18 -0500

    Created test auth server

---
+++ gateway/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# This is a barebones auth webhook.
+# It lets any user log in with any password
+# Not to be used for anything other than testing
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/password", methods=["POST"])
+def handle_password():
+    data = request.json
+    username = data["username"]    
+
+    return jsonify({"sucess": True, "authenticatedUsername": username})
+
+if __name__ == "__main__":
+    app.run()
