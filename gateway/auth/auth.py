d604da39e106a515d7d3b8549a0d10bcb101f30c
KRI3G <kevin.guerra.v@gmail.com>
2025-08-02 03:00:15 -0500

    Fixed typo and set flask to listen on all addresses

--- gateway/auth/auth.py
+++ gateway/auth/auth.py
@@ -8,12 +8,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/health")
+def health():
+    return "Ok"
+
 @app.route("/password", methods=["POST"])
 def handle_password():
     data = request.json
     username = data["username"]    
 
-    return jsonify({"sucess": True, "authenticatedUsername": username})
+    return jsonify({"success": True, "authenticatedUsername": username})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=1337)
