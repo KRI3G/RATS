f8825bb4894127d7e89735445fbddedff36d0aba
KRI3G <kevin.guerra.v@gmail.com>
2025-08-04 00:01:44 -0500

    Testing uid configuration from host???

--- gateway/generate_keys.sh
+++ gateway/generate_keys.sh
@@ -5,3 +5,5 @@ ssh-keygen -q -N "" -t ed25519 -f ssh_host_ed25519_key
 
 # Public key files are not needed
 rm ssh_host_*_key.pub
+
+# WATCH THE UID:GID SETTINGS, they must match containerssh's running user
