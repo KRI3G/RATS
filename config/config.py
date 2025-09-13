a54fca73f2384ed745174c2b728586b10d365dd7
KRI3G <kevin.guerra.v@gmail.com>
2025-09-12 21:10:38 -0500

    Fixed typo

--- config/config.py
+++ config/config.py
@@ -29,7 +29,7 @@ def handle_json_request():
         print(f"[CONFIG] Creating home directory for {authenticated_username}")
     folder_path = os.path.abspath(folder_path)
 
-    return jsonify({"config": {"docker": {"execution": {"host": {"binds": [f"{folder_path}:/home/{authenticatedUsername}:z"]}}}}})
+    return jsonify({"config": {"docker": {"execution": {"host": {"binds": [f"{folder_path}:/home/{authenticated_username}:z"]}}}}})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1337)
