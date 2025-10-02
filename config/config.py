05571076dbeb493d454a67f6806fca8af03cc847
KRI3G <150230145+KRI3G@users.noreply.github.com>
2025-10-02 16:18:04 -0500

    testing init process

--- config/config.py
+++ config/config.py
@@ -45,10 +45,10 @@ def handle_json_request():
                         ]
                     },
                     "idleCommand": [
-                        "/bin/sh"
+                        "/sbin/init"
                     ],
                     "shellCommand": [
-                        "/bin/sh"
+                        "/sbin/init"
                     ]
                     #"idleCommand": [
                     #    "/bin/sh", "-c", f"/usr/sbin/useradd {authenticated_username} -s /bin/bash && /usr/bin/su - {authenticated_username}"
