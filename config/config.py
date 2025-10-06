f285077c54a59f6f07b6a024b33fcd3aedc88d10
Kevin G. <kevin.guerra.v@gmail.com>
2025-10-06 13:30:37 -0500

    Revert "Testing config mount"
    This reverts commit 981050b85b6b5c02326d8d783fd670ea2a521358.

--- config/config.py
+++ config/config.py
@@ -21,46 +21,14 @@ def handle_json_request():
     authenticated_username = data.get('authenticatedUsername')
     
     # Check if the user home folder exists, otherwise create it
-    # folder_path = os.path.join('/users/', authenticated_username)
-    # if not os.path.exists(folder_path):
-    #     os.umask(0o000)
-    #     os.makedirs(folder_path, mode=0o777, exist_ok=True)
-    #     os.umask(0o022)
-    #     print(f"[CONFIG] Creating home directory for {authenticated_username}")
-    # folder_path = os.path.abspath(folder_path)
-
-    # This depends on where your RATS is installed
-    return jsonify({
-        "config": {
-            "docker": {
-                "execution": {
-                    "host": {
-                        "binds": [
-                            f"/opt/RATS/users/{authenticated_username}:/home/{authenticated_username}:z"
-                        ]
-                    }, 
-                    #"container": {
-                    #    #"cmd": [
-                    #    #    f"/usr/sbin/useradd {authenticated_username} -s /bin/bash && && /usr/bin/su - {authenticated_username} && exit"
-                    #    #]
-                    #    #"user": "student"
-                    #}
-                    #"idleCommand": [
-                    #    "/sbin/init"
-                    #],
-                    #"shellCommand": [
-                    #    "/sbin/init"
-                    #]
-                    #"idleCommand": [
-                    #    "/bin/sh", "-c", f"/usr/sbin/useradd {authenticated_username} -s /bin/bash && /usr/bin/su - {authenticated_username}"
-                    #],
-                    #"shellCommand": [
-                    #    "/bin/sh", "-c", f"/usr/sbin/useradd {authenticated_username} -s /bin/bash && /usr/bin/su - {authenticated_username}"
-                    #]
-                }
-            }
-        }
-    })
+    folder_path = os.path.join('/users/', authenticated_username)
+    if not os.path.exists(folder_path):
+        os.umask(0o000)
+        os.makedirs(folder_path, mode=0o777, exist_ok=True)
+        os.umask(0o022)
+        print(f"[CONFIG] Creating home directory for {authenticated_username}")
+    #folder_path = os.path.abspath(folder_path)
+    return jsonify({"config": {"docker": {"execution": {"host": {"binds": [f"{folder_path}:/home/{authenticated_username}:z"]}}}}})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1337)
